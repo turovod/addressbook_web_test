e1861329d709719dd43ca9b14143895dbe136759
Sergey <turovodspb@gmail.com>
2020-08-27 22:21:31 +0300

    Value Object

---
+++ AddressbookWebTest/AddressbookWebTest/AccountData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressbookWebTest
+{
+    class AccountData
+    {
+        string username = "admin";
+        string password = "secret";
+
+        public AccountData (string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+
+            set { password = value; }
+        }
+    }
+}
