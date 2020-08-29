6baf9b0095c2fcdc25937da2ff4cf13db4418b71
Sergey <turovodspb@gmail.com>
2020-08-29 20:10:50 +0300

    Contacts, release

--- AddressbookWebTest/AddressbookWebTest/ContactsData.cs
+++ AddressbookWebTest/AddressbookWebTest/ContactsData.cs
@@ -8,6 +8,45 @@ namespace AddressbookWebTest
 {
     class ContactsData
     {
+        string firstname;
+
+        public ContactsData(string firstname)
+        {
+            this.firstname = firstname;
+        }
+
+        public string Firstname 
+        {
+            get { return firstname; }
+            set { firstname = value; } 
+        }
+
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string Nickname { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string Home { get; set; }
+        public string Mobile { get; set; }
+        public string Work { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+        public string Homepage { get; set; }
+        public string Bday { get; set; }
+        public string Bmonth { get; set; }
+        public string Byear { get; set; }
+        public string Aday { get; set; }
+        public string Amonth { get; set; }
+        public string Ayear { get; set; }
+        public string Address2 { get; set; }
+        public string Phone2 { get; set; }
+        public string Notes { get; set; }
+
+
+
 
     }
 }
