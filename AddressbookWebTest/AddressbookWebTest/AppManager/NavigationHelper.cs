83d572abf4e320be88124f4269e3737437b9b3e5
Sergey <turovodspb@gmail.com>
2020-09-06 22:46:14 +0300

    Optimisation navigator

--- AddressbookWebTest/AddressbookWebTest/AppManager/NavigationHelper.cs
+++ AddressbookWebTest/AddressbookWebTest/AppManager/NavigationHelper.cs
@@ -13,26 +13,48 @@ namespace AddressbookWebTest
 
         public void OpenHomePage()
         {
+            if (appManager.Driver.Url == baseURL && appManager.IsElementPresent.IsElementPresent(By.LinkText("Edit")))
+            {
+                return;
+            }
+
             driver.Navigate().GoToUrl(baseURL);
         }
 
         public void GoToHomePage()
         {
+            if (appManager.Driver.Url == baseURL && appManager.IsElementPresent.IsElementPresent(By.LinkText("Edit")))
+            {
+                return;
+            }
+
             driver.FindElement(By.LinkText("home")).Click();
         }
 
         public void ReturnGropsPage()
         {
+            if (appManager.Driver.Url == baseURL + "group.php" && appManager.IsElementPresent.IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("group page")).Click();
         }
 
         public void GoToGroupsPage()
         {
+            if (appManager.Driver.Url == baseURL + "group.php" && appManager.IsElementPresent.IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
         public void GoToNewContacts()
         {
+            if (appManager.IsElementPresent.IsElementPresent(By.XPath("//div[@id='content']/h1")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("add new")).Click();
         }
     }
