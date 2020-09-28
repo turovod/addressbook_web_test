3387cc7a79dc4527c49ac0515dc025df8d1f242e
Sergey <turovodspb@gmail.com>
2020-09-28 19:44:49 +0300

    Added to linq2db.MySql  library

---
+++ AddressbookWebTest/AddressbookWebTest/Model/AddressbookDB.cs
@@ -0,0 +1,18 @@
+﻿using LinqToDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressbookWebTest
+{
+    // for using sql DB add linq2db.MySql
+    public class AddressbookDB : LinqToDB.Data.DataConnection
+    {
+        public AddressbookDB() : base("AddressBook") { }
+
+        public ITable<GroupData> Groups { get { return GetTable<GroupData>(); } }
+        public ITable<ContactsData> Contacts { get { return GetTable<ContactsData>(); } }
+    }
+}
